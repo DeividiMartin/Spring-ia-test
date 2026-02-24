7dcb7e7d4e7ce76bd21a03c837ee207932b674ad
Deividi Martin <deividi_97@hotmail.com>
2026-02-24 18:29:52 -0300

    more optoins @Postmappiong

--- src/main/java/com/example/chatgpt/controller/OpenAIController.java
+++ src/main/java/com/example/chatgpt/controller/OpenAIController.java
@@ -2,11 +2,17 @@ package com.example.chatgpt.controller;
 
 import com.example.chatgpt.model.MensagemDTO;
 import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.input.Prompt;
+import dev.langchain4j.model.input.PromptTemplate;
+import dev.langchain4j.model.openai.OpenAiChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class OpenAIController {
@@ -22,5 +28,25 @@ public class OpenAIController {
 
         return model.generate(mensagem.mensagem());
     }
+
+    @PostMapping("/chat4")
+    public String conversarComGPT4(@RequestBody MensagemDTO mensagem) {
+
+        ChatLanguageModel model = new OpenAiChatModel.OpenAiChatModelBuilder()
+                .apiKey("openai.api.key")
+                .temperature(0.5)
+                .modelName("gpt-4o").build();
+        return model.generate(mensagem.mensagem());
+    }
+
+    @PostMapping("/tamplate")
+    public String conversarComGPT4Tamplate (@RequestBody MensagemDTO mensagem) {
+        PromptTemplate tamplate = PromptTemplate.from("Você é um programador Java. Vovê deve responder essa pergunta: {{question}}");
+         Map<String, Object > mapa = new HashMap<String, Object>();
+        mapa.put("question", mensagem.mensagem());
+        Prompt prompt = tamplate.apply(mapa);
+        return model.generate(mensagem.mensagem());
+    }
+
 
 }
